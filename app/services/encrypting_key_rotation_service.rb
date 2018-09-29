d9e75bd5b7f224935310ffda75ffa7f7dec2c63d
Anthony Bobsin <bobsinj@gmail.com>
2018-09-29 15:52:34 -0400

    Wrap EncryptingKeyRotationService#rotate_all method in DB transaction to keep data integrity when exceptions occur

--- app/services/encrypting_key_rotation_service.rb
+++ app/services/encrypting_key_rotation_service.rb
@@ -7,21 +7,24 @@ class EncryptingKeyRotationService
   end
 
   def rotate_all
-    new_key = DataEncryptingKey.generate!(primary: true)
-    old_keys = DataEncryptingKey.where.not(id: new_key.id)
+    # wrap in transaction to ensure our encrypted data integrity
+    ActiveRecord::Base.transaction do
+      new_key = DataEncryptingKey.generate!(primary: true)
+      old_keys = DataEncryptingKey.where.not(id: new_key.id)
 
-    old_keys.each do |old_key|
-      # fetch encrypted strings in batches
-      # start from 0 each time because we're emptying this dataset
-      encrypted_strings_for_key(old_key).find_in_batches(start: 0) do |batch|
-        # rotate each encrypted object with new key
-        batch.each { |encrypted| rotate(encrypted, new_key) }
+      old_keys.each do |old_key|
+        # fetch encrypted strings in batches
+        # start from 0 each time because we're emptying this dataset
+        encrypted_strings_for_key(old_key).find_in_batches(start: 0) do |batch|
+          # rotate each encrypted object with new key
+          batch.each { |encrypted| rotate(encrypted, new_key) }
+        end
       end
-    end
 
-    # delete all keys not in-use
-    old_keys.delete_all
-    new_key
+      # delete all keys not in-use
+      old_keys.delete_all
+      new_key
+    end
   end
 
   def rotate(encrypted, new_key = DataEncryptingKey.generate!)
