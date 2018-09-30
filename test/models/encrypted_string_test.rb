38c9523146ea2cef0d0b7395b7e59e682a49c556
Anthony Bobsin <bobsinj@gmail.com>
2018-09-30 16:41:57 -0400

    improve test coverage for controllers and models

--- test/models/encrypted_string_test.rb
+++ test/models/encrypted_string_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class EncryptedStringTest < ActiveSupport::TestCase
+  test '.encryption_encrypted_key' do
+    primary_key = data_encrypting_keys(:primary)
+    secondary_key = data_encrypting_keys(:secondary)
 
-  test 'Missing tests! Please fix' do
+    # when model does not have a data_encrypting_key
+    string = EncryptedString.create!(value: "Test One")
+    assert string.encryption_encrypted_key == primary_key.encrypted_key
 
+    # when model does have data_encrypting_key
+    string = EncryptedString.create!(value: "Test Two", data_encrypting_key_id: secondary_key.id)
+    assert string.encryption_encrypted_key == secondary_key.encrypted_key
   end
 end
